99f88d8706357133ee70c3d83137197b4faed800
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-07 12:15:26 +0300

    feat: update Navbar component styles, add wallet connection button, and enhance Tailwind CSS configuration

--- frontend/components/app/landing/Navbar.tsx
+++ frontend/components/app/landing/Navbar.tsx
@@ -3,22 +3,44 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="hidden md:flex justify-between items-center bg-red-500">
-      <div>
-        <h1 className="text-5xl font-extrabold text-gray-700 logo ">
+    <div className="flex justify-between items-center">
+      <div className="mx-2">
+        <h1 className="text-2xl font-extrabold text-gray-700 logo ">
           pamojaFI.
         </h1>
       </div>
-      <div className="flex items-center justify-between">
-        <Link to="/dashboard">Dashboard</Link>
+      <div className="hidden md:flex items-center justify-between text-gray-700 title gap-2">
+        <Link
+          to="/dashboard"
+          className="text-lg mx-2 hover:border-b-[2px] py-1 hover:scale-95 ease-in-out duration-150"
+        >
+          Dashboard
+        </Link>
 
-        <Link to="/create">Create</Link>
+        <Link
+          to="/create"
+          className="text-lg mx-2 hover:border-b-[2px] py-1 hover:scale-95 ease-in-out duration-150"
+        >
+          Create
+        </Link>
 
-        <Link to="/pending">Pending</Link>
+        <Link
+          to="/pending"
+          className="text-lg mx-2 hover:border-b-[2px] py-1 hover:scale-95 ease-in-out duration-150"
+        >
+          Pending
+        </Link>
 
-        <Link to="/community">Community</Link>
+        <Link
+          to="/community"
+          className="text-lg mx-2 hover:border-b-[2px] py-1 hover:scale-95 ease-in-out duration-150"
+        >
+          Community
+        </Link>
       </div>
-      <div></div>
+      <div>
+        <button className="text-red-500 text-sm title">Connect wallet</button>
+      </div>
     </div>
   );
 }
