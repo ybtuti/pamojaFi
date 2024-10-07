a8a07e5a0ee37b2b498183aa7f25c68cb3a1540d
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-07 11:02:23 +0300

    frontend init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,18 +15,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="font-logo">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="logo">
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="title">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
