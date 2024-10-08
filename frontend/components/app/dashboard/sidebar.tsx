e803f7e68864dc27377a51b058ede60c1e74e7be
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-08 19:53:08 +0300

    Refactor DashboardSidebar component to remove unnecessary styles

--- frontend/components/app/dashboard/sidebar.tsx
+++ frontend/components/app/dashboard/sidebar.tsx
@@ -84,7 +84,7 @@ export function DashboardSidebar() {
 const Dashboard = () => {
   return (
     <div className="flex flex-1">
-      <div className="p-2 md:p-10 rounded-tl-2xl border-[1px] border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+      <div className="p-2 md:p-10 md:rounded-tl-2xl border-[1px] border-neutral-200 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex gap-2">
           {[...new Array(4)].map((i) => (
             <div
