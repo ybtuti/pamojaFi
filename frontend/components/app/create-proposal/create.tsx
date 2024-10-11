41dbf6783161f55ec485322d85d98ac4560763ec
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-11 22:53:50 +0300

    feat: create a project proposal form with data validation

--- frontend/components/app/create-proposal/create.tsx
+++ frontend/components/app/create-proposal/create.tsx
@@ -50,7 +50,7 @@ function CreateProposalForm() {
     console.log(values);
   }
   return (
-    <div>
+    <div className="mx-4">
       <div>
         <h1 className="my-2 logo text-3xl font-semibold text-benefits">
           Create Proposal
@@ -64,21 +64,220 @@ function CreateProposalForm() {
       </div>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Wakulima Market" {...field} />
-                </FormControl>
-                <FormDescription>Title of your project</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
+          <div className="grid md:grid-cols-2 gap-4 mb-8">
+            <div className="flex flex-col gap-3">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Wakulima Market"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Title of your project
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="projectLink"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">Project Link</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="https://wakulima-market.com"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Link to your project
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="projectHeaderImageLink"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">Header Image</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="https://mkulima.png"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Image url of your project
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="projectCategory"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">Category</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Crop Farming"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Category of your project
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">Description</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="What does your project entail?"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Description of your project
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <FormField
+                control={form.control}
+                name="targetAmountETH"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">Target</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="0.6 ETH"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Target amount in ETH
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="walletAddress"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">
+                      Project Wallet Address
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="0x123abc45739892783618hjdghyuwe783"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Link to your project
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="projectTeamInformation"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">Team Information</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Describe the team behind your project"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Information about your team
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="otherLinks"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">Relevant Links</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Links to your project"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Other relevant links
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="moreProjectDetails"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="logo">Additional Details</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Additional details about your project"
+                        className="title"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-[#808080]">
+                      Additional project information
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <Button
+            type="submit"
+            className="w-full bg-benefits text-hero logo my-4"
+          >
+            Submit
+          </Button>
         </form>
       </Form>
     </div>
