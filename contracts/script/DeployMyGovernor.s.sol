3219dcfa4c6aeed8b4e7ea9d670a3e50f2cd5dc8
ybtuti <samuelmsuu@gmail.com>
2024-10-19 14:52:11 +0300

    refactor: Remove unnecessary code in DeployDaoModule script

--- contracts/script/DeployMyGovernor.s.sol
+++ contracts/script/DeployMyGovernor.s.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Script} from "forge-std/Script.sol";
-import {MyGovernor} from "../src/daoModule/MyGovernor.sol";
-import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
-import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
-import {console} from "forge-std/console.sol";
+// import {Script} from "forge-std/Script.sol";
+// import {MyGovernor} from "../src/daoModule/MyGovernor.sol";
+// import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+// import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+// import {console} from "forge-std/console.sol";
 
-contract DeployMyGovernor is Script {
-    function run() external {
-        address deployer = msg.sender;
+// contract DeployMyGovernor is Script {
+//     function run() external {
+//         address deployer = msg.sender;
 
-        // Replace these with actual addresses or logic to get the addresses
-        IVotes token = IVotes();
-        TimelockController timelock = TimelockController();
+//         // Replace these with actual addresses or logic to get the addresses
+//         IVotes token = IVotes();
+//         TimelockController timelock = TimelockController();
 
-        vm.startBroadcast();
+//         vm.startBroadcast();
 
-        MyGovernor governor = new MyGovernor(token, timelock);
+//         MyGovernor governor = new MyGovernor(token, timelock);
 
-        vm.stopBroadcast();
+//         vm.stopBroadcast();
 
-        console.log("MyGovernor deployed to:", address(governor));
-    }
-}
+//         console.log("MyGovernor deployed to:", address(governor));
+//     }
+// }
